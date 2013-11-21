52217caaa077be3a848e67cf3717a7686cd587be
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2013-11-21 12:16:21 +0200

    Detect packages depending on any installed packages.

--- scripts/manually-installed-deps.sh
+++ scripts/manually-installed-deps.sh
@@ -2,7 +2,15 @@
 # Generate a list of manually installed dependencies on this system
 # with parameter -a, mark them autoinstalled
 
-if [ -n "$1" -a "$1" == "-a" ]; then
+if [ -n "$1" -a "$1" == "-h" -o "$1" == "--help" ]; then
+  echo "Usage: manually-installed-deps.sh [-a|-f]
+    -a will mark automatically installed any package depending on a manually installed package.
+    -f will mark automatically installed any package depending on an installed package.
+"
+elif [ -n "$1" -a "$1" == "-f" ]; then
+    deps=$( aptitude search "?installed?not(?automatic)?reverse-depends(?installed)" -F "%p" )
+    sudo aptitude markauto $deps
+elif [ -n "$1" -a "$1" == "-a" ]; then
     deps=$( aptitude search "?installed?not(?automatic)?reverse-depends(?installed?not(?automatic))" -F "%p" )
     sudo aptitude markauto $deps
 else
