ed6cba555b3552f6a8f2231892c3cd425d115bfb
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2017-05-20 17:25:52 +0800

    New version of saferm.

--- scripts/saferm.sh
+++ scripts/saferm.sh
@@ -9,7 +9,7 @@
 ## Last update Sat Aug 16 23:49:18 2008 Eemil Lagerspetz
 ##
 
-version="1.12";
+version="1.15";
 
 ## flags (change these to change default behaviour)
 recursive="" # do not recurse into directories by default
@@ -19,7 +19,7 @@ unsafe="" # do not behave like regular rm by default
 
 ## possible flags (recursive, verbose, force, unsafe)
 # don't touch this unless you want to create/destroy flags
-flaglist="r v f u"
+flaglist="r v f u q"
 
 # Colours
 blue='\e[1;34m'
@@ -32,14 +32,13 @@ trash_desktops="$HOME/.local/share/Trash/files"
 # if neither is running:
 trash_fallback="$HOME/Trash"
 
-# which trashbin?
-use_gnome=$( ps -U $USER | grep gnome-settings )
-use_kde=$( ps -U $USER | grep startkde )
+# use .local/share/Trash?
+use_desktop=$( ps -U $USER | grep -E "gnome-settings|startkde|mate-session|mate-settings|mate-panel|gnome-shell" )
 
 # mounted filesystems, for avoiding cross-device move on safe delete
 filesystems=$( mount | awk '{print $3; }' )
 
-if [ -n "$use_gnome" -o -n "$use_kde" ]; then
+if [ -n "$use_desktop" ]; then
     trash="${trash_desktops}"
     infodir="${trash}/../info";
     for k in "${trash}" "${infodir}"; do
@@ -49,8 +48,8 @@ else
     trash="${trash_fallback}"
 fi
 
-function usagemessage {
-	echo -e "This is ${blue}saferm.sh$norm $version. Will ask to unsafe-delete instead of cross-fs
+usagemessage() {
+	echo -e "This is ${blue}saferm.sh$norm $version. Better Gnome3 detection. Will ask to unsafe-delete instead of cross-fs
     move. Allows unsafe (regular rm) delete (ignores trashinfo).
 Detects gnome better. Creates trash and trashinfo directories if they do not exist.
 Handles symbolic link deletion. Does not complain about different user any more.\n";
@@ -58,12 +57,13 @@ Handles symbolic link deletion. Does not complain about different user any more.
 	echo -e "$blueOPTIONS$norm:"
 	echo -e "$blue-r$norm      allows recursively removing directories."
 	echo -e "$blue-f$norm      Allow deleting special files (devices, ...)."
-    echo -e "$blue-u$norm      Unsafe mode, bypass trash and delete files permanently."
+  echo -e "$blue-u$norm      Unsafe mode, bypass trash and delete files permanently."
 	echo -e "$blue-v$norm      Verbose, prints more messages. Default in this version."
+  echo -e "$blue-q$norm      Quiet mode. Opposite of verbose."
 	echo "";
 }
 
-function detect {
+detect() {
     if [ ! -e "$1" ]; then fs=""; return; fi
     path=$(readlink -f "$1")
     for det in $filesystems; do
@@ -77,7 +77,7 @@ function detect {
 }
 
 
-function trashinfo {
+trashinfo() {
 #gnome: generate trashinfo:
 	bname=$( basename -- "$1" )
     fname="${trash}/../info/${bname}.trashinfo"
@@ -88,14 +88,14 @@ DeletionDate=$( date +%Y-%m-%dT%H:%M:%S )
 EOF
 }
 
-function setflags {
+setflags() {
     for k in $flaglist; do
 	reduced=$( echo "$1" | sed "s/$k//" )
 	if [ "$reduced" != "$1" ]; then
 	    flags_set="$flags_set $k"
 	fi
     done
-    for k in $flags_set; do
+  for k in $flags_set; do
 	if [ "$k" == "v" ]; then
 	    verbose="true"
 	elif [ "$k" == "r" ]; then 
@@ -104,83 +104,102 @@ function setflags {
 	    force="true"
 	elif [ "$k" == "u" ]; then 
 	    unsafe="true"
+	elif [ "$k" == "q" ]; then 
+    unset verbose
 	fi
-    done
+  done
 }
 
-function deletefiles {
-    for k in "$@"; do
-	fdesc="$blue$k$norm";
-    	act="";
-    	msg="";
-    	if [ ! -e "$k" -a ! -L "$k" ]; then # does not exist
-    	    msg="File does not exist:"
-    	elif [ ! -w "$k" -a ! -L "$k" ]; then # not writable
-    	    msg="File is not writable:"
-    	#elif [ ! -O "$k" ]; then # not owned by us
-    		#if [ -n "$force" ]; then
-    			#act="true" # delete only if forced
-    		#else
-    	    	#msg="File is not owned by $USER (use -f to override):"
-    	    #fi
-    	elif [ ! -f "$k" -a ! -d "$k" ]; then # Special or sth else.
-    	    if [ -n "$force" ]; then 
-	        	act="true" # delete only if forced.
-    	    else
-	        	msg="Is not a regular file or directory (and -f not specified):"
-    	    fi
-    	elif [ -f "$k" ]; then # is a file
-	        act="true" # operate on files by default
-    	elif [ -d "$k" ]; then # is a dir
-    	    if [ -n "${recursive}" ]; then
-    			act="true" # operate on dirs if r specified
-    		else
-    			msg="Is a directory (and -r not specified):"
-    		fi
-		else
-			# not file or dir. This branch should not be reached.
-			msg="No such file or directory:";
-    	fi
-    
-    	#actual action:    
-    	if [ -n "$act" ]; then # if we can delete
-			if [ -n "$verbose" ]; then # print if requested
-			    if [ -n "$unsafe" ]; then 
-	    		    echo -e "Deleting $red$k$norm"
-	    		else
-                    detect "$PWD/$k"
-                    if [ "${fs}" == "${tfs}" ]; then
-        	    		echo -e "Moving $blue$k$norm to $red${trash}$norm"
-                    else
-                        unset answer;
-                        until [ "$answer" == "y" -o "$answer" == "n" ]; do
-                            echo -e "$blue$k$norm is on $blue${fs}$norm. Unsafe delete (y/n)?"
-                            read -n 1 answer;
-                        done
-                        if [ "$answer" == "y" ]; then
-                            unsafe="yes"
-        	    		    echo -e "Deleting $red$k$norm"
-                        else
-        	    		    echo -e "Moving $blue$k$norm to $red${trash}$norm"
-                        fi
-                    fi
-    	    	fi
-	    	fi
+performdelete() {
 			# "delete" = move to trash
-			if [ -n "$unsafe" ]; then
-			    #UNSAFE: permanently remove files.
-			    rm -rf -- "$k"
+			if [ -n "$unsafe" ]
+			then
+			  if [ -n "$verbose" ];then echo -e "Deleting $red$1$norm"; fi
+		    #UNSAFE: permanently remove files.
+		    rm -rf -- "$1"
 			else
-			    mv -b -- "$k" "${trash}" # moves and backs up old files
-			    if [ -n "$use_gnome" -o -n "$use_kde" ]; then
-		    	        # generate trashinfo for desktop environments
-	    		        trashinfo "$k"
-			    fi
+			  if [ -n "$verbose" ];then echo -e "Moving $blue$k$norm to $red${trash}$norm"; fi
+		    mv -b -- "$1" "${trash}" # moves and backs up old files
 			fi
-		else
-			# not acting, instead print error message:
-			echo -e "$msg $fdesc.";
+}
+
+askfs() {
+  detect "$PWD/$1"
+  if [ "${fs}" != "${tfs}" ]; then
+    unset answer;
+    until [ "$answer" == "y" -o "$answer" == "n" ]; do
+      echo -e "$blue$1$norm is on $blue${fs}$norm. Unsafe delete (y/n)?"
+      read -n 1 answer;
+    done
+    if [ "$answer" == "y" ]; then
+      unsafe="yes"
+    fi
+  fi
+}
+
+complain() {
+  msg=""
+  if [ ! -e "$1" -a ! -L "$1" ]; then # does not exist
+    msg="File does not exist:"
+	elif [ ! -w "$1" -a ! -L "$1" ]; then # not writable
+    msg="File is not writable:"
+	elif [ ! -f "$1" -a ! -d "$1" -a -z "$force" ]; then # Special or sth else.
+    	msg="Is not a regular file or directory (and -f not specified):"
+	elif [ -f "$1" ]; then # is a file
+    act="true" # operate on files by default
+	elif [ -d "$1" -a -z "${recursive}" ]; then
+			msg="Is a directory (and -r not specified):"
+	else
+		# not file or dir. This branch should not be reached.
+		msg="No such file or directory:"
+	fi
+}
+
+asknobackup() {
+  unset answer
+	until [ "$answer" == "y" -o "$answer" == "n" ]; do
+	  echo -e "$blue$k$norm could not be moved to trash. Unsafe delete (y/n)?"
+	  read -n 1 answer
+	done
+	if [ "$answer" == "y" ]
+	then
+	  unsafe="yes"
+	  performdelete "${k}"
+	  ret=$?
+		# Reset temporary unsafe flag
+	  unset unsafe
+	  unset answer
+	else
+	  unset answer
+	fi
+}
+
+deletefiles() {
+  for k in "$@"; do
+	  fdesc="$blue$k$norm";
+	  complain "${k}"
+	  if [ -n "$msg" ]
+	  then
+		  echo -e "$msg $fdesc."
+    else
+    	#actual action:
+    	if [ -z "$unsafe" ]; then
+    	  askfs "${k}"
     	fi
+		  performdelete "${k}"
+		  ret=$?
+		  # Reset temporary unsafe flag
+		  if [ "$answer" == "y" ]; then unset unsafe; unset answer; fi
+      echo "MV exit status: $ret"
+      if [ ! "$ret" -eq 0 ]
+      then 
+        asknobackup "${k}"
+      fi
+      if [ -n "$use_desktop" ]; then
+          # generate trashinfo for desktop environments
+        trashinfo "${k}"
+      fi
+    fi
 	done
 }
 
